118507680c30d76d4f60c644de6a7d4b57fc1984
unknown <cristiandiaz89@outlook.com>
2021-12-20 17:51:28 -0300

    file

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -273,7 +273,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var cambioADolar(euro*1.20);
+  var cambioADolar = (euro*1.20);
   return cambioADolar;
 }
 
@@ -284,8 +284,20 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  if(letra.length > 1){
+    return "opcion Incorrecta";
+    
+  }
+  if(letra == "a" || letra =="e"|| letra == "i"|| letra =="o"|| letra =="u"){
+    
+    return "es vocal";
+  }
+  return "opcion incorrecta";
   
+  
+
 }
+
 
 
 
